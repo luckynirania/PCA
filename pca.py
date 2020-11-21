19f99c24da06b12093772040e59b3cb791bba654
Lokesh Nirania <lknmahan@gmail.com>
2020-11-21 17:31:53 +0530

    merge pca class into main

---
+++ pca.py
@@ -0,0 +1,8 @@
+import copy
+import numpy as np
+
+
+
+
+# q = PCA(1)
+# q.fit(mat)
--- program.py
+++ program.py
@@ -1,11 +1,54 @@
-import os
+import sys, os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import imageio
+import copy
 
-image_path = "Datasets/Group 6 Prateek Jain and Lokesh Kumar Nirania"
+if len(sys.argv) < 3:
+    print("usuage : python3 program.py <dataset-path> <n_components>")
+    sys.exit()
+
+n_components = int(sys.argv[2])
+
+class PCA:
+    n_components = 0
+    components_ = []
+    mean_record = []
+    def __init__(self, n_components):
+        self.n_components = n_components
+    # will give us n_components principal components
+    def apply(self, mat):
+        # making copy our matrix
+        matrix = copy.deepcopy(mat.to_numpy())
+
+        # column mean subtraction
+        self.mean_record = matrix.mean(axis=0, keepdims=True)
+        X = matrix - matrix.mean(axis=0, keepdims=True)
+
+        print(X.shape)
+
+        C = np.cov(X,rowvar=False)
+
+        print(C.shape)
+
+        u, s, vh = np.linalg.svd(C, hermitian=True)
+
+        print(u)
+        print(s)
+        print(u.shape, s.shape, vh.shape)
+
+        pca_mat = u[:,:10]
+        print('test', pca_mat.shape)
+        k_mat = u[:,:self.n_components]
+
+        print(k_mat.shape)
+
+        self.components_ = pca_mat.transpose()
+        self.projected = np.dot(np.dot(X, k_mat), k_mat.transpose()) + self.mean_record
+
+image_path = sys.argv[1]
 
 image_files = sorted([os.path.join(image_path, file)
     for file in os.listdir(image_path) if file.endswith('.png')])
@@ -18,25 +61,31 @@ for each in image_files:
     face = pd.Series(img.flatten(),name=each)
     faces = faces.append(face)
 
+print(faces.iloc[0].values.reshape(64,64))
+
 fig, axes = plt.subplots(3,10,figsize=(9,3), subplot_kw={'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace=0.01, wspace=0.01))
 for i, ax in enumerate(axes.flat):
     ax.imshow(faces.iloc[i].values.reshape(64,64),cmap="gray")
-plt.show()
+fig.suptitle('Original Images')
+# plt.show()
 
 
-from sklearn.decomposition import PCA
 #n_components=0.80 means it will return the Eigenvectors that have the 80% of the variation in the dataset
-faces_pca = PCA(n_components=10)
-faces_pca.fit(faces)
+faces_pca = PCA(n_components)
+faces_pca.apply(faces)
+
+# print(faces_pca.components_[0].reshape(64,64))
 
 fig, axes = plt.subplots(1,10,figsize=(9,3), subplot_kw={'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace=0.01, wspace=0.01))
 for i, ax in enumerate(axes.flat):
     ax.imshow(faces_pca.components_[i].reshape(64,64),cmap="gray")
-plt.show()
+fig.suptitle('Visualization of 10 principal components')
+# plt.show()
 
-components = faces_pca.transform(faces)
-projected = faces_pca.inverse_transform(components)
+# components = faces_pca.transform(faces)
+# projected = faces_pca.inverse_transform(components)
 fig, axes = plt.subplots(3,10,figsize=(9,3), subplot_kw={'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace=0.01, wspace=0.01))
 for i, ax in enumerate(axes.flat):
-    ax.imshow(projected[i].reshape(64,64),cmap="gray")
+    ax.imshow(faces_pca.projected[i].reshape(64,64),cmap="gray")
+fig.suptitle('Reconstruction using ' + str(n_components) + ' componentes')
 plt.show()
