5c564ba392814c53bb7ef15747f81a8ccf484271
prateekjain2606 <jprateek2606@gmail.com>
2020-11-22 00:57:18 +0530

    Updated Code

--- program.py
+++ program.py
@@ -1,3 +1,5 @@
+# importing dependencies required in the code
+
 import sys, os
 import pandas as pd
 import numpy as np
@@ -6,86 +8,87 @@ import matplotlib.image as mpimg
 import imageio
 import copy
 
+# Usage error, in case user passes wrong arguments 
 if len(sys.argv) < 3:
     print("usuage : python3 program.py <dataset-path> <n_components>")
     sys.exit()
 
 n_components = int(sys.argv[2])
 
+
+# Class to perform PCA
 class PCA:
-    n_components = 0
-    components_ = []
-    mean_record = []
+    # Default initialization of variables
+    n_components = 0 # Stores number of components to be used for PCA
+    components_ = [] # Stores first 10 principal components for visualizing the features they represent
+    mean_record = [] # Stores the mean of each column in data
     def __init__(self, n_components):
-        self.n_components = n_components
-    # will give us n_components principal components
+        self.n_components = n_components # Initiallizing with input from user
     def apply(self, mat):
-        # making copy our matrix
+        # making copy of input data
         matrix = copy.deepcopy(mat.to_numpy())
 
-        # column mean subtraction
+        # centering the data by subtracting column mean
         self.mean_record = matrix.mean(axis=0, keepdims=True)
         X = matrix - matrix.mean(axis=0, keepdims=True)
 
-        print(X.shape)
+        print("Shape of Input matrix " + str(X.shape))
 
-        C = np.cov(X,rowvar=False)
+        C = np.cov(X,rowvar=False) # Calculating Covariance of input matrix
 
-        print(C.shape)
+        print("Shape of Covariance matrix " + str(C.shape))
 
-        u, s, vh = np.linalg.svd(C, hermitian=True)
+        u, s, vh = np.linalg.svd(C, hermitian=True) # Performing SVD on Covariance matrix using python library
+        # u stores principal components column-wise in descending order of eigen values
 
-        print(u)
-        print(s)
-        print(u.shape, s.shape, vh.shape)
-
-        pca_mat = u[:,:10]
-        print('test', pca_mat.shape)
-        k_mat = u[:,:self.n_components]
-
-        print(k_mat.shape)
-
+        # storing 10 principal components to visualize the features they represent
+        pca_mat = u[:,:10] 
         self.components_ = pca_mat.transpose()
-        self.projected = np.dot(np.dot(X, k_mat), k_mat.transpose()) + self.mean_record
 
+        # Performing compression
+        k_mat = u[:,:self.n_components] # Storing first n_components
+        compressed = np.dot(X, k_mat) # Calculating and storing compressed data
+
+        # Reconstructing the images
+        self.reconstructed = np.dot(compressed, k_mat.transpose()) # Recalculating data in uncompressed form
+        self.reconstructed += self.mean_record # Adding mean of columns to the data to bring it in original form
+
+# Storing image path
 image_path = sys.argv[1]
 
+# Storing the names of all the images in the dataset directory whose path is given by user
 image_files = sorted([os.path.join(image_path, file)
     for file in os.listdir(image_path) if file.endswith('.png')])
 
-# print(image_files)
+# Creating a dataframe to store data
 faces = pd.DataFrame([])
 
+# Storing the input data
 for each in image_files:
-    img = imageio.imread(each)
-    face = pd.Series(img.flatten(),name=each)
-    faces = faces.append(face)
+    img = imageio.imread(each) # Reading the image
+    face = pd.Series(img.flatten(),name=each) # Flattening the image
+    faces = faces.append(face) # Appending it to the matrix storing data
 
-print(faces.iloc[0].values.reshape(64,64))
-
+# Displaying Original Images
 fig, axes = plt.subplots(3,10,figsize=(9,3), subplot_kw={'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace=0.01, wspace=0.01))
 for i, ax in enumerate(axes.flat):
     ax.imshow(faces.iloc[i].values.reshape(64,64),cmap="gray")
 fig.suptitle('Original Images')
-# plt.show()
 
 
-#n_components=0.80 means it will return the Eigenvectors that have the 80% of the variation in the dataset
-faces_pca = PCA(n_components)
-faces_pca.apply(faces)
+# Performing PCA by calling functions of PCA class
+faces_pca = PCA(n_components) # Initializing the object of PCA class
+faces_pca.apply(faces) # Calling the function which performs PCA
 
-# print(faces_pca.components_[0].reshape(64,64))
-
+# Displaying features represented by first 10 principal components
 fig, axes = plt.subplots(1,10,figsize=(9,3), subplot_kw={'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace=0.01, wspace=0.01))
 for i, ax in enumerate(axes.flat):
     ax.imshow(faces_pca.components_[i].reshape(64,64),cmap="gray")
 fig.suptitle('Visualization of 10 principal components')
-# plt.show()
 
-# components = faces_pca.transform(faces)
-# projected = faces_pca.inverse_transform(components)
+# Displaying reconstructed images
 fig, axes = plt.subplots(3,10,figsize=(9,3), subplot_kw={'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace=0.01, wspace=0.01))
 for i, ax in enumerate(axes.flat):
-    ax.imshow(faces_pca.projected[i].reshape(64,64),cmap="gray")
+    ax.imshow(faces_pca.reconstructed[i].reshape(64,64),cmap="gray")
 fig.suptitle('Reconstruction using ' + str(n_components) + ' componentes')
 plt.show()
